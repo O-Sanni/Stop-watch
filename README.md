ebf22a1fbeb6dda3ead24bebc148a264dde076d1
Olga_Sanni <dev.sannikov@gmail.com>
2020-03-25 19:47:33 -0400

    working restart  and start

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Create separate javascript files for three buttons, start, pause and restart; create another javascript file which would call the button classes and set some functions on click.
 
 https://reactjs.org/
+https://upmostly.com/tutorials/setinterval-in-react-components-using-hooks
--- src/components/Watch.js
+++ src/components/Watch.js
@@ -6,38 +6,45 @@ import PauseButton from "./PauseButton";
 
 class Watch extends React.Component{
     constructor(){
-        super();
-        this.state={min: 0,
-        seconds:50, countPause:1};
+        super(); 
+        //make the states default minutes is set to 0, seconds to 0 and count for pause to 0
+        this.state={min:0,
+        seconds:0, countPause:0, countStart:1, start:false };
     }
-    getMin=()=>{
-
-    }
-    startClick =()=>{
-        this.inter=setInterval(()=>{
-            this.setState({
-                seconds:(this.state.seconds +1),
-            });
-            if(this.state.seconds>=60){
-                this.setState({seconds: 0,
-                min: this.state.min +1});
-            }
-        },1000);
-     }
-
-    restartClick=()=>{
+   
+    startClick =()=>{ //function which called by pressing Start button
         
-     this.setState({timer:0});
+        //this.inter will use setInterval to give new values to state every 1000 m.second
+        if(this.state.countStart===1){
+        this.inter=setInterval(()=>{
+            this.setState({ //set state values
+                seconds:(this.state.seconds +1), //will add 1 to seconds every 1000 m.s
+            });
+            if(this.state.seconds>=60){ //if the value of seconds is more or equals to 60 
+                this.setState({seconds: 0, //seconds will be set to 0
+                min: this.state.min +1}); // and minutes will increase by 1
+            }
+        },1000); 
+     }
+     this.setState({start:true, countStart:this.state.countStart+1}); //set countStart to true and add 1 to countStart
     }
-    
+     //by pressing restart button state will be updated to original state which was 0's
+    restartClick=()=>{ 
+     this.setState({min: 0,
+        seconds:0, countPause:0, countStart:1, start:false });
+        clearInterval(this.inter); //clearInterval to stop the setInterval()
+    }
+    // by pressing Pause we updating countPause by adding 1
     pauseClick=()=>{
+        if(this.state.start===true){
         this.setState({countPause: (this.state.countPause+1)});
-        if(this.state.countPause%2===0){
+        if(this.state.countPause%2!=0){ //if countPause not divisiable by 2 we call startClick function to continue setInterval
+            this.setState({countStart:1})
             this.startClick();
         }
-        else{
+        else{ //if countPause divisiable by 2 we use clearInterval to stop the setInterval()
       clearInterval(this.inter);}
-
+        }
      }
     
     render(){
